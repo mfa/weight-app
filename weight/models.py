0b7db2fa375cf125f58c8fb826544c42f7235e9f
mfa <andreas@madflex.de>
2012-05-28 23:18:30 +0200

    models for scales and weight

--- weight/models.py
+++ weight/models.py
@@ -45,3 +45,72 @@ class User(db.Model):
 
     def __repr__(self):
         return '<User %s %s>' % (self.username, self.email)
+
+
+class Scale(db.Model):
+    """
+    """
+
+    #:
+    id = db.Column(db.UnicodeText,
+                   unique=True, primary_key=True)
+
+    #:
+    owner = db.Column(db.UnicodeText)
+
+    #:
+    model = db.Column(db.UnicodeText)
+
+    #:
+    comment = db.Column(db.UnicodeText)
+
+
+    #: creation date, set on creation
+    createdate = db.Column(db.DateTime(timezone=True),
+                           db.DefaultClause(db.func.now()),
+                           nullable=False)
+    #: modification date, set on every change
+    lastmoddate = db.Column(db.DateTime(timezone=True),
+                            db.DefaultClause(db.func.now()),
+                            nullable=False)
+    #: changed_by
+    changed_by = db.Column(db.UnicodeText,
+                           db.DefaultClause(db.func.session_user()))
+
+
+class Weight(db.Model):
+    """
+    """
+
+    id = db.Column(db.Integer, db.Sequence(),
+                   unique=True, primary_key=True)
+
+    #: date of weighing
+    date = db.Column(db.Date(timezone=True),
+                     db.DefaultClause(db.func.today()),
+                     nullable=False)
+
+    #: day of week (as string), could be an enum
+    weekday = db.Column(db.UnicodeText)
+
+    #:
+    scale_id = db.Column(db.UnicodeText,
+                         db.ForeignKey(scale.id))
+
+    #:
+    weight = db.Column(db.Float)
+
+    #:
+    comment = db.Column(db.UnicodeText)
+
+    #: creation date, set on creation
+    createdate = db.Column(db.DateTime(timezone=True),
+                           db.DefaultClause(db.func.now()),
+                           nullable=False)
+    #: modification date, set on every change
+    lastmoddate = db.Column(db.DateTime(timezone=True),
+                            db.DefaultClause(db.func.now()),
+                            nullable=False)
+    #: changed_by
+    changed_by = db.Column(db.UnicodeText,
+                           db.DefaultClause(db.func.session_user()))
