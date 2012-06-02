179757870af3e23d705cb17e4573436b29fcd7d6
mfa <andreas@madflex.de>
2012-06-03 00:47:49 +0200

    login form should not show correct usernames

--- weight/forms.py
+++ weight/forms.py
@@ -20,18 +20,14 @@ class LoginForm(Form):
         super(LoginForm, self).__init__(*args, **kwargs)
         self.next.data = request.args.get('next', None)
 
-    def validate_username(form, field):
-        from models import User
-        u1 = User.query.get(field.data)
-        if not u1:
-            raise ValidationError('User not found')
-
     def validate_password(form, field):
         from models import User
         u1 = User.query.get(form.username.data)
         if u1:
             if not u1.check_password(field.data):
-                raise ValidationError('Password wrong')
+                raise ValidationError('Username or Password wrong')
+        else:
+            raise ValidationError('Username or Password wrong')
 
 
 class ProfileForm(Form):
@@ -45,6 +41,8 @@ class ProfileForm(Form):
                              [EqualTo('confirm', message='Passwords must match')])
     confirm  = PasswordField('Repeat Password')
     submit = SubmitField("Save")
+
+ 
 
 
 class WeightForm(Form):
