00eeaf603e16ea747a813aa84c5c8db92373fbcf
mfa <andreas@madflex.de>
2012-06-03 20:44:25 +0200

    added backrefs, comments and cascades to models

--- weight/models.py
+++ weight/models.py
@@ -26,6 +26,11 @@ class User(db.Model):
     default_scale_name = db.Column(db.UnicodeText,
                                    db.ForeignKey("scale.name"))
 
+    #: backref from Weight table
+    weights = db.relationship('Weight',
+                              backref='users',
+                              lazy='select')
+
     #: creation date, set on creation
     createdate = db.Column(db.DateTime(timezone=True),
                            db.DefaultClause(db.func.now()),
@@ -54,21 +59,36 @@ class User(db.Model):
 
 
 class Scale(db.Model):
-    """
+    """ scales usable for weighting
     """
 
-    #:
+    #: given name of scale
     name = db.Column(db.UnicodeText,
                      unique=True, primary_key=True)
 
-    #:
+    #: owner of the scale
     owner = db.Column(db.UnicodeText)
 
-    #:
+    #: model and/or manufacturer
     model = db.Column(db.UnicodeText)
 
-    #:
+    #: comment for scale
     comment = db.Column(db.UnicodeText)
+
+
+    #: backref from User table
+    users = db.relationship('User',
+                            backref='scales',
+                            lazy='select',
+                            cascade="all",
+                            passive_updates=False)
+
+    #: backref from Weight table
+    weights = db.relationship('Weight',
+                              backref='scales',
+                              lazy='select',
+                              cascade="all",
+                              passive_updates=False)
 
     #: creation date, set on creation
     createdate = db.Column(db.DateTime(timezone=True),
@@ -87,7 +107,7 @@ class Scale(db.Model):
 
 
 class Weight(db.Model):
-    """
+    """ Weights for day and user with a given scale
     """
 
     id = db.Column(db.Integer, db.Sequence('weight_id_seq'),
@@ -108,18 +128,18 @@ class Weight(db.Model):
         dt = self.wdate
         return dt.strftime("%a")
 
-    #:
+    #: scale used for weighting
     scale_name = db.Column(db.UnicodeText,
                            db.ForeignKey("scale.name"))
 
-    #:
+    #: weight of user at given day
     weight = db.Column(db.Float)
 
-    #:
+    #: weighted user
     user_username = db.Column(db.UnicodeText,
                               db.ForeignKey("user.username"))
 
-    #:
+    #: comment for this weighting
     comment = db.Column(db.UnicodeText)
 
     #: creation date, set on creation
