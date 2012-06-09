a88e05a134e43106bc57af88a11d812ef811b1e7
mfa <andreas@madflex.de>
2012-06-09 22:26:38 +0200

    added test for adding weight; some code cleanup

--- weight/tests/test_forms.py
+++ weight/tests/test_forms.py
@@ -18,42 +18,66 @@ class FormTest(unittest.TestCase):
     def setUpClass(self):
         with app.test_request_context():
             db.create_all()
-            u = models.User(username=self.username, 
+            u = models.User(username=self.username,
                             email=u'')
             u.set_password(self.password)
             db.session.add(u)
             db.session.commit()
 
+
     def test_login_ok(self):
-        """ testing a correct login 
+        """ testing a correct login
         """
         with FlaskClient(app, response_wrapper=FormWrapper) as client:
-            response = client.get('/login')
+            response = client.get('/login', follow_redirects=True)
 
             response.form.fields['username'] = self.username
             response.form.fields['password'] = self.password
             response.form.fields['remember'] = False
 
-            response = response.form.submit(client)
-
+           response = response.form.submit(client)
             self.assertEqual(response.status, '302 FOUND')
+            # because of session self.login can't be used
             self.assertEqual(session.get('user_id'), self.username)
+
+
+    def login(self, username, password):
+        """ login to site with username and password;
+        """
+        with FlaskClient(app, response_wrapper=FormWrapper) as client:
+            response = client.get('/login', follow_redirects=True)
+
+            response.form.fields['username'] = username
+            response.form.fields['password'] = password
+            response.form.fields['remember'] = False
+
+            response = response.form.submit(client, follow_redirects=True)
+            return response
 
 
     def test_login_fail(self):
         """ testing the login with wrong password
         """
-        with FlaskClient(app, response_wrapper=FormWrapper) as client:
-            response = client.get('/login')
-
-            response.form.fields['username'] = self.username
-            response.form.fields['password'] = u'wrong'
-            response.form.fields['remember'] = False
-
-            response = response.form.submit(client)
-
+        with app.test_request_context():
+            response = self.login(self.username, u'wrong')
             self.assertEqual(response.status, '200 OK')
             self.assertNotEqual(session.get('user_id'), self.username)
 
 
-        
+    def test_weight_add(self):
+        """ test adding one weight dataset
+        """
+        with FlaskClient(app, response_wrapper=FormWrapper) as client:
+
+            self.login(self.username, self.password)
+            with client.session_transaction() as sess:
+                sess['user_id'] = self.username
+
+            response = client.get('/weight/add/',
+                                  follow_redirects=True)
+
+            response.form.fields['weight'] = "10.0"
+
+            response = response.form.submit(client)
+
+            self.assertEqual(response.status, '200 OK')
