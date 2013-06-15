9e9c5715bddbf5b8b87ccfeb0b0cb2f4969dcc5a
Andreas Madsack <andreas@madflex.de>
2013-06-15 16:23:20 +0200

    added flash message on fitbit push

--- weight/views.py
+++ weight/views.py
@@ -159,6 +159,7 @@ def weight(wid=None):
             if u1.fitbit_user_key is not None and \
                     u1.fitbit_user_secret is not None:
                 fitbit_push(u1, elem.wdate, elem.weight)
+                flash('Data pushed to fitbit', 'info')
 
         if elem:
             if elem.scale_name:
